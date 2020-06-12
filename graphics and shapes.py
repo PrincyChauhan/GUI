b45c2d4c3c970bf4afb453473a02bf5b393c6e46
PrincyChauhan <66620521+PrincyChauhan@users.noreply.github.com>
2020-06-12 22:09:42 +0530

    Add files via upload

---
+++ graphics and shapes.py
@@ -0,0 +1,13 @@
+from tkinter import*
+import tkinter.messagebox
+root = Tk()
+
+
+canvas=Canvas(root,width=200,height=100)
+canvas.pack()
+blackLine=canvas.create_line(0,0,200,50)
+redLine=canvas.create_line(0,100,200,50,fill="red")
+greenBox=canvas.create_rectangle(25,25,130,60,fill="green")
+
+canvas.delete(ALL)
+root.mainloop()
